a909084abb3c155f2ac670c11dcccd9455b3fd33
YYInc <3067845479@qq.com>
2019-02-22 12:45:23 +0800

    fix: 修改获取不到npm运行命令传进行来的key参数问题

--- script/commandar-watch.js
+++ script/commandar-watch.js
@@ -1,10 +1,4 @@
 const program = require('commander')
-const CliService = require('@vue/cli-service/lib/Service')
-const Service = new CliService(process.cwd())
-Service.init()
-
-// 获取 vue-cli 默认的 webpack 配置
-const webpackConfig = Service.resolveWebpackConfig()
 
 program.version('0.1.0')
   .option('-k, --key [type]', 'key to build')
@@ -14,8 +8,14 @@ program
   .description('run the given remote command')
   .action(function(){
     console.log('watch 命令注册成功')
-    // 用户传进来的 key 值
-    console.log(program.key)
-  })
+
+    /**
+     * 用户传进来的 key 值
+     * yarn 运行命令时，可通过 program.key 获取到用户传进来的 key 值
+     * npm 运行命令时，需通过 process.env.npm_config_key 获取到用户传进来的 key 值
+     */
+    const key = program.key ? program.key : process.env.npm_config_key
+    console.log(key)
+})
 
 program.parse(process.argv)
